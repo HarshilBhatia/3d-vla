c3770c8bfecae316158864be072e236e659b5251
Harshil Singh Bhatia <harshilb@grogu.ri.cmu.edu>
2026-01-26 01:17:18 -0500

    -

--- run_single_task_zarr.sh
+++ run_single_task_zarr.sh
@@ -11,13 +11,6 @@ TASKS="bimanual_lift_tray"
 RAW_ROOT=$(python3 paths.py RAW_ROOT)
 ZARR_ROOT=$(python3 paths.py ZARR_ROOT)
 
-# Set up CoppeliaSim environment
-export COPPELIASIM_ROOT=/home/lzaceria/mscv/3dvla/3d-vla/PyRep/CoppeliaSim_Edu_V4_1_0_Ubuntu20_04
-export LD_LIBRARY_PATH=$COPPELIASIM_ROOT:$LD_LIBRARY_PATH
-export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIASIM_ROOT
-
-# Use the full path to the python executable in the 3dfa environment
-PYTHON_EXE=/home/lzaceria/miniconda3/envs/3dfa/bin/python
 
 # =========================
 # PROCESS EACH TASK
@@ -30,7 +23,7 @@ for TASK in "${ADDR[@]}"; do
 
     # 1. Download Raw Data
     echo "[STEP 1] Checking/Downloading raw data..."
-    ${PYTHON_EXE} scripts/rlbench/download_peract2.py --root ${RAW_ROOT} --tasks ${TASK}
+    python scripts/rlbench/download_peract2.py --root ${RAW_ROOT} --tasks ${TASK}
 
     # 2. Generate Zarr
     ZARR_OUT=${ZARR_ROOT}/${TASK}
@@ -40,7 +33,7 @@ for TASK in "${ADDR[@]}"; do
     rm -rf ${ZARR_OUT}/train.zarr ${ZARR_OUT}/val.zarr
     mkdir -p ${ZARR_OUT}
 
-    ${PYTHON_EXE} data_processing/peract2_to_zarr.py \
+    python data_processing/peract2_to_zarr.py \
       --root ${RAW_ROOT} \
       --tgt ${ZARR_OUT} \
       --tasks ${TASK}
